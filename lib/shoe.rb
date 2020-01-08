b93584431b816d061019146be956c0d4623c7e4d
Tyler Caprioli <capriolityler@gmail.com>
2020-01-08 13:00:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,11 @@ class Shoe
     @brand = brand
   end
   def cobble
+<<<<<<< HEAD
     puts "Your shoe is as good as new!"
+=======
+    puts "this shoe has been repaired"
+>>>>>>> 96d1e85fc4bba559d95a0188a935225d44bd815f
     @condition = "new"
   end 
 end 
